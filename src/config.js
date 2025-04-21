65d376eb81d6eca6192dc343b1dcfa48fed3e4e0
Alishon <alison.martinez@soy.sena.edu.co>
2025-04-21 10:54:37 -0500

    Se hizo merge de una rama a la rama master

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/qfinder-notas";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5174";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
